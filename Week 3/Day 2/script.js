8e219318022737d7bf586f9bb4210e05c797ebd3
aritocohen <arielco@payoneer.com>
2020-08-27 19:43:58 +0300

    after exercise from day 8 completed

--- Week 3/Day 2/script.js
+++ Week 3/Day 2/script.js
@@ -10,8 +10,58 @@
 // 	console.log(y);
 // }
 
-var newDog = 'Chihuahua';
 
-var nbletter = newDog.length;
 
-console.log(nbletter)
+// var newDog = 'Chihuahua';
+
+// var ndletter = newDog.length;
+
+// console.log(ndletter)
+
+// var Upnd = newDog.toUpperCase();
+
+// console.log(Upnd)
+
+
+
+// if (newDog === 'Chihuahua') {
+
+// 	console.log('I LOVE Chihuahua, its my favorite dog');
+
+// } else{
+
+// 	console.log('I dont care, I prefer CATS');
+// }
+
+
+
+// var numb = 8;
+
+// if (isNaN(numb)) {
+
+// 	console.log('It is not a number');
+// }
+// else{
+// 	console.log('It is a number');
+// }
+
+
+
+var numbOfUsers = 8;
+var classOnline = ['Emma', 'David', 'Nadav'];
+// var rest = numbOfUsers - 2;
+
+console.log(numbOfUsers);
+
+if (numbOfUsers === 0) {
+	console.log('no one online');
+}
+else if (numbOfUsers === 1) {
+	console.log(classOnline[0], 'online');
+}
+else if (numbOfUsers === 2) {
+	console.log(classOnline[0], 'and', classOnline[1], 'online')
+}
+else{
+	console.log(classOnline[0], classOnline[1], 'and 6 more are online')
+}
