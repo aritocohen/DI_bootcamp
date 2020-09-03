b9597d7185f7b085dd035e465ea5b23b58811b9e
aritocohen <arielco@payoneer.com>
2020-09-03 20:36:25 +0300

    calculator

---
+++ Week 4/Day 12/main.js
@@ -0,0 +1,13 @@
+// var btns = document.querySelectorAll("button");
+// var result = document.getElementById("result");
+
+//     for (var thisBtn of btns) {
+//         thisBtn.addEventListener('click', function() {
+//         if (this.innerHTML == "=")
+//             result.value = eval(result.value);
+//         else if (this.innerHTML == "clear")
+//             result.value = "";
+//         else
+//             result.value += this.innerHTML;
+//                 })
+//             }
