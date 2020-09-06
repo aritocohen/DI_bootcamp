46e6909caa97723b647c781a591b27b77ed132bb
aritocohen <arielco@payoneer.com>
2020-09-06 21:16:32 +0300

    todo

--- Week 4/Day 12/main.js
+++ Week 4/Day 12/main.js
@@ -11,3 +11,25 @@
 //             result.value += this.innerHTML;
 //                 })
 //             }
+
+let calcstrg = "";
+let display = document.getElementById('display');
+	console.log(display);
+
+function my_f (btn){
+	calcstrg = calcstrg + btn;
+	display.innerHTML = calcstrg;
+	console.log(calcstrg)
+}
+
+function result(){
+	let calcresult = eval(calcstrg);
+	display.innerHTML = calcresult;
+	console.log(calcstrg + "+" + calcresult);
+	calcstrg = calcresult;
+}
+
+function calcclear(){
+	calcstrg = '';
+	display.innerHTML = 0;
+}
