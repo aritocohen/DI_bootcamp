3892adfb78575f41c355c4b39502d37a814d825c
aritocohen <arielco@payoneer.com>
2020-10-08 20:06:04 +0300

    practice

---
+++ Week 8/Day 23/Main.py
@@ -0,0 +1,60 @@
+# EX
+
+# number = 1
+#
+# while number <= 10:
+#     print(number)
+#     number += 1
+#
+# print('Done!')
+
+
+# current_number = 0
+# while current_number <= 10:
+#     current_number += 1
+#     if 3 < current_number < 7: # If the number is between 3 and 7
+#         continue                # Go back to the beginning of the loop
+#     print(current_number)
+
+
+
+
+# def calculation(first_num, second_num):
+#     return (int(first_num) + int(second_num))
+#
+# final_num = calculation(8, 5)
+# print(final_num)
+
+
+# EX 1 XP Functions
+
+# def display_message():
+#     print("I'm learning functions")
+#
+# display_message()
+
+
+# EX 2 XP
+
+# def favorite_book(title):
+#     print("One of my favorite books is " + title)
+#
+# favorite_book("The lord of the rings")
+
+
+# EX 3 XP
+
+# def make_shirt(size, text):
+#     print("Your shirt's size is " + size + " and it will say: " + text)
+
+# make_shirt("xl", "Im coding")
+
+# make_shirt(text="HOLA", size="L")
+
+
+# def make_shirt(size= "Large", text= "I love Pyhton"):
+#     print("Your shirt's size is " + size + " and it will say: " + text)
+#
+# make_shirt()
+
+
