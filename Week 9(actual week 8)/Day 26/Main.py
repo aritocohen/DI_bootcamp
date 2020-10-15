687ece9e7c945eb07c55ec8e4e891f67672463c5
aritocohen <arielco@payoneer.com>
2020-10-15 20:26:07 +0300

    wuju

---
+++ Week 9(actual week 8)/Day 26/Main.py
@@ -0,0 +1,74 @@
+# XP EX W8 cl 1
+
+# class Cat:
+#     species = 'mammal'
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def oldest_cat(self):
+#         for cat in cats:
+#             if age
+#
+#
+# cats = []
+# cat_1 = Cat("Meow", 3)
+# cat_2 = Cat("Tom", 7)
+# cat_3 = Cat("Sylvester", 2)
+#
+# cats.append(cat_1)
+# cats.append(cat_2)
+# cats.append(cat_3)
+
+
+# EX 2
+
+# class Dog:
+#     def __init__(self, name_dog, height_dog):
+#         self.name_dog       = name_dog
+#         self.height_dog     = height_dog
+#
+#     def talk(self):
+#         print("Wouaf")
+#
+#     def jump(self):
+#         self.height_dog = self.height_dog*2
+#         print(f'{self.name_dog} jumps {self.height_dog} cms tall')
+#
+# davids_dog = Dog("Rex", 50)
+#
+# davids_dog.talk()
+# davids_dog.jump()
+#
+# sarahs_dog = Dog("Teacup", 20)
+#
+# sarahs_dog.talk()
+# sarahs_dog.jump()
+#
+# def bigger_dog():
+#     if davids_dog.height_dog > sarahs_dog.height_dog:
+#         print(f'{davids_dog.name_dog} is bigger')
+#    elif:
+#         print(f'{sarahs_dog.name_dog} is bigger')
+
+
+# Class 2
+
+# class Circle:
+#     color = "red"
+#
+#     def __init__(self, diameter):
+#         self.diameter = diameter
+#
+#     def grow(self, factor=2):
+#         self.diameter = self.diameter * factor
+#
+#     def get_color(self):
+#        return Circle.color
+#
+# circle1 = Circle(2)
+# print(circle1.color)
+# print(Circle.color)
+# print(circle1.get_color())
+# circle1.grow(3)
+# print(circle1.diameter)
